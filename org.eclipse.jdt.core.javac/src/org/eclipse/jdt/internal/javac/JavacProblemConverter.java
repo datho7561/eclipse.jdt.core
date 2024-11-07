63ce6bae6e8148d707b733d077e89df44d8b1c91
David Thompson <davthomp@redhat.com>
2024-11-07 09:17:58 -0500

    Error IDs for problems unique to javac
    This causes 6 regerssions because 'possible this escape' is now
    reported.
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/JavacProblemConverter.java
+++ org.eclipse.jdt.core.javac/src/org/eclipse/jdt/internal/javac/JavacProblemConverter.java
@@ -1080,6 +1080,10 @@ public class JavacProblemConverter {
 			case "compiler.err.void.not.allowed.here" -> IProblem.ParameterMismatch;
 			case "compiler.err.abstract.cant.be.accessed.directly" -> IProblem.DirectInvocationOfAbstractMethod;
 			case "compiler.warn.annotation.method.not.found" -> IProblem.UndefinedAnnotationMember;
+			case "compiler.warn.possible.this.escape" -> JavacProblemIds.PossibleThisEscape;
+			case "compiler.warn.possible.this.escape.location" -> JavacProblemIds.PossibleThisEscapeLocation;
+			case "compiler.warn.possible.loss.of.precision" -> JavacProblemIds.PossibleLossOfPrescision;
+			case "compiler.warn.auxiliary.class.accessed.from.outside.of.its.source.file" -> JavacProblemIds.AuxiliaryClassAccessedOutsideItsSourceFile;
 			default -> {
 				ILog.get().error("Could not accurately convert diagnostic (" + diagnostic.getCode() + ")\n" + diagnostic);
 				if (diagnostic.getKind() == javax.tools.Diagnostic.Kind.ERROR && diagnostic.getCode().startsWith("compiler.err")) {
